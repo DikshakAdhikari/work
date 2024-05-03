6343a82057342013be17df8f2e3c01c25eb69760
hunktuck <ddikshakk@gmail.com>
2024-05-03 11:44:02 +0530

    video download done

--- video.py
+++ video.py
@@ -1,1 +1,22 @@
-print("dy")
+from pytube import YouTube # type: ignore
+
+def download_video(url, save_path):
+    try:
+      
+        yt = YouTube(url)
+        
+
+        stream = yt.streams.get_highest_resolution()
+     
+        stream.download(output_path=save_path)
+        
+        print("Download completed successfully!")
+    except Exception as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    video_url = "https://www.youtube.com/watch?v=iiaH2q-RlDs"  
+    save_location = "./downloads" 
+    
+    download_video(video_url, save_location)
