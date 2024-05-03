ea068acd6f63300a96c4869d50d96a3a8abb82c2
hunktuck <ddikshakk@gmail.com>
2024-05-03 15:16:28 +0530

    transcripting done

--- transcript.py
+++ transcript.py
@@ -2,6 +2,7 @@ from deepgram import DeepgramClient, PrerecordedOptions
 from getAudio import extractAudio
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
@@ -9,27 +10,30 @@ extractAudio(url="https://www.youtube.com/watch?v=iiaH2q-RlDs", location="./subt
 
 
 DEEPGRAM_API_KEY = os.getenv("API_KEY")
-print(DEEPGRAM_API_KEY)
 
 
-AUDIO_URL = {
-  'url': 'https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav'
-}
+def deepGram_api_call(audio_url):
+    url = "https://api.deepgram.com/v1/listen"
+    headers = {
+    "Authorization": "Token "+ DEEPGRAM_API_KEY,
+    "content-type": "application/json"
+    }
+    data = {
+    "url": audio_url,
+    "smart_format": True,
+    "model": "nova-2",
+    "language": "en-GB"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    return response
+
 
 def main():
-    deepgram = DeepgramClient(DEEPGRAM_API_KEY)
+    AUDIO_URL = 'https://static.deepgram.com/examples/Bueller-Life-moves-pretty-fast.wav'
+    transcripted_result= deepGram_api_call(audio_url= AUDIO_URL)
+    print(transcripted_result.json())
 
-    options = PrerecordedOptions(
-        smart_format=True, model="nova-2", language="en-GB"
-    )
-
-    print('Requesting transcript...')
-    print('Your file may take up to a couple minutes to process.')
-    print('While you wait, did you know that Deepgram accepts over 40 audio file formats? Even MP4s.')
-    print('To learn more about customizing your transcripts check out developers.deepgram.com')
-
-    response = deepgram.listen.prerecorded.v('1').transcribe_url(AUDIO_URL, options)
-    print(response.to_json(indent=4))
-
+   
 if __name__ == '__main__':
     main()
